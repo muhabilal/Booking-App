8d4f46c339f276aeaa27b96cb5bae48c93c8c357
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-16 02:11:59 +0500

    Button component with props

---
+++ Components/ButtonCmp.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Button, extendTheme, NativeBaseProvider} from 'native-base';
+const ButtonCmp = (props: any) => {
+  const text = props.btnText;
+  const theme = extendTheme({
+    colors: {
+      primary: {
+        200: '#32B768',
+        _text: {
+          color: '#FFFFFF',
+        },
+      },
+    },
+  });
+  return (
+    <NativeBaseProvider theme={theme}>
+      <Button bg={'green.400'}>{text}</Button>
+    </NativeBaseProvider>
+  );
+};
+
+export default ButtonCmp;
