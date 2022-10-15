339a8bed68f0e8c67823344c64868b3d23ebe4cd
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-15 15:52:27 +0500

    Button component added

---
+++ Components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from 'native-base';
+const Button = () => {
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default Button;
