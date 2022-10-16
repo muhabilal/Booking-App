b90312260409e4981faebd3f1d76960d81c17173
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-16 18:01:00 +0500

    import register screen

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import First from './screens/Splash';
 import ButtonCmp from './Components/ButtonCmp';
+import InputsCmp from './Components/InputsCmp';
+import Register from './screens/Register';
 const App = () => {
   return (
     <NativeBaseProvider>
-      <First />
+      {/* <First /> */}
       {/* <ButtonCmp /> */}
+      {/* <InputsCmp /> */}
+      <Register />
     </NativeBaseProvider>
   );
 };
