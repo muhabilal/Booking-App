c2f2b91c174c48ae02e7dc766b355d583e34de2b
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-17 16:14:59 +0500

    button use

--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,12 +1,39 @@
-import {Stack, Text} from 'native-base';
-import React from 'react';
+import {
+  NativeBaseProvider,
+  Stack,
+  Center,
+  Image,
+  HStack,
+  Row,
+} from 'native-base';
+import React, {useState} from 'react';
 import InputsCmp from '../Components/InputsCmp';
+import Press from '../Components/Press';
 
 const Register = () => {
+  const [isTrue, setIsTrue] = useState(true);
   return (
-    <Stack justifyContent="center" alignItems="center">
-      <InputsCmp name="Fullname" Placeholder="Enter name" />
-    </Stack>
+    <NativeBaseProvider>
+      <Stack flex={1} bg={'#D9D9D9'}>
+        <Center>
+          <Image
+            mt={5}
+            resizeMode="cover"
+            source={require('../assests/logo.png')}
+            alt="Alternate Text"
+          />
+        </Center>
+        <Stack flex={1} bg={'#FFFFFF'} borderTopRadius={30}>
+          <Row justifyContent={'space-around'}>
+            <Press txt="Create Account" />
+            <Press txt="Login" />
+          </Row>
+        </Stack>
+      </Stack>
+    </NativeBaseProvider>
+    // <Stack justifyContent="center" alignItems="center">
+    //   <InputsCmp name="Fullname" Placeholder="Enter name" />
+    // </Stack>
   );
 };
 
