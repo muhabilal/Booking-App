08e8524b06194544839f6b63398a5a835f854ee5
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-17 12:32:46 +0500

    splash screen complete

--- screens/Splash.tsx
+++ screens/Splash.tsx
@@ -13,87 +13,41 @@ import {Image} from 'native-base';
 import ButtonCmp from '../Components/ButtonCmp';
 const First = () => {
   return (
-    <Box flex="1" safeAreaTop>
-      <ScrollView>
-        <Stack>
-          <Stack mt={20}>
-            <Center>
-              <Image
-                source={require('../assests/logo.png')}
-                alt="Alternate Text"
-              />
-            </Center>
-          </Stack>
-          <Stack mt={20}>
-            <VStack alignItems="center" justifyContent="center">
-              <Heading size="2xl">Welcome</Heading>
-              <Text mt={4}>Before enjoying Foodmedia services</Text>
-              <Text>Please register first</Text>
-            </VStack>
-          </Stack>
-          <Stack mt={12}>
-            <VStack space={3} justifyContent="center" alignItems="center">
-              <ButtonCmp btnText="Create Account" />
-              <ButtonCmp btnText="Login" />
-            </VStack>
-          </Stack>
-          <VStack mt={2} alignItems="center" justifyContent="center">
-            <Text fontSize="xs">
-              By logging in or registering,you have agreed to the
-              <Text color={'#32B768'}>Terms and</Text>
-            </Text>
-            <Text fontSize="xs">
-              <Text color={'#32B768'}>Conditions</Text>and{' '}
-              <Text color={'#32B768'}>Privacy Policy.</Text>
-            </Text>
+    <Box flex={'1'} bg={'white'}>
+      <Stack>
+        {/* <ScrollView> */}
+        <Stack mt={20}>
+          <Center>
+            <Image
+              source={require('../assests/logo.png')}
+              alt="Alternate Text"
+            />
+          </Center>
+        </Stack>
+        <Stack mt={20}>
+          <VStack alignItems="center" justifyContent="center">
+            <Heading size="2xl">Welcome</Heading>
+            <Text mt={4}>Before enjoying Foodmedia services</Text>
+            <Text>Please register first</Text>
           </VStack>
         </Stack>
-      </ScrollView>
-      <Box flex={'1'} bg={'white'}>
-        <Stack mb={20}>
-          {/* <ScrollView> */}
-          <Stack mt={20}>
-            <Center>
-              <Image
-                source={require('../assests/logo.png')}
-                alt="Alternate Text"
-              />
-            </Center>
-          </Stack>
-          <Stack mt={20}>
-            <VStack alignItems="center" justifyContent="center">
-              <Heading size="2xl">Welcome</Heading>
-              <Text mt={4}>Before enjoying Foodmedia services</Text>
-              <Text>Please register first</Text>
-            </VStack>
-          </Stack>
-          <Stack mt={12}>
-            <VStack space={3} justifyContent="center" alignItems="center">
-              <ButtonCmp
-                btnText="Create Account"
-                color_txt="#10B981"
-                background={'#32B768'}
-              />
-              <ButtonCmp
-                btnText="Login"
-                background={'#D1FAE5'}
-                color_txt="white"
-              />
-            </VStack>
-          </Stack>
-          <VStack mt={2} mb={10} alignItems="center" justifyContent="center">
-            <Text fontSize="xs">
-              By logging in or registering,you have agreed to the
-              <Text color={'#32B768'}>Terms and</Text>
-            </Text>
-            <Text fontSize="xs">
-              <Text color={'#32B768'}>Conditions</Text>and{' '}
-              <Text color={'#32B768'}>Privacy Policy.</Text>
-            </Text>
+        <Stack mt={12}>
+          <VStack space={3} justifyContent="center" alignItems="center">
+            <ButtonCmp btnText="Create Account" background={'#32B768'} />
+            <ButtonCmp btnText="Login" background={'#D1FAE5'} />
           </VStack>
-          {/* </ScrollView> */}
         </Stack>
-      </Box>
+        <VStack mt={2} mb={10} alignItems="center" justifyContent="center">
+          <Text fontSize="xs">
+            By logging in or registering,you have agreed to the
+            <Text color={'#32B768'}>Terms and</Text>
+          </Text>
+          <Text fontSize="xs">
+            <Text color={'#32B768'}>Conditions</Text>and{' '}
+            <Text color={'#32B768'}>Privacy Policy.</Text>
+          </Text>
+        </VStack>
+      </Stack>
     </Box>
   );
 };
