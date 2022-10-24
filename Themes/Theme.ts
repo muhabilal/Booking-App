139645f672e086cec50e8c34f41b60faa38fed74
M Bilal <bilaldarmuhammad@gmail.com>
2022-10-24 11:18:24 +0500

    update theme and solve error

--- Themes/Theme.ts
+++ Themes/Theme.ts
@@ -2,11 +2,12 @@ import { extendTheme } from "native-base";
 
  const Theme = extendTheme({
     colors: {
-      primary: '#32B768',
-      secondary:'#D1FAE5',
-      singletons:"#FFFFFF",
-      light:{50:'#D9D9D9',100:'#F4F4F4'},
-      black:'#222222'
+      primary: {10:'rgb(50, 183, 104)'},
+      secondary:{10:"rgb(209,250,229)"},
+      singletons:"rgb(255,255,255)",
+      light:{50:'rgb(217,217,217)',100:'rgb(244,244,244)'},
+      black:'rgb(34,34,34)',
+      ternary:{50:"rgb(235,70,70)"},
     },
   });
 export default Theme
